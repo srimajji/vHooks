f2a26a9c9a10b7a72fbf85b2b9f385e814555b74
Sri Majji <sri.majji@live.com>
2019-04-15 07:06:01 -0700

    init nextjs integration

--- src/Server.ts
+++ src/Server.ts
@@ -4,43 +4,62 @@ import * as http from "http";
 import { Socket } from "socket.io";
 import { logger } from "./utils/Logger";
 import events from "./services/EventService";
+import next from "next";
 
-export const server = http.createServer(app);
-const io = require("socket.io")(server);
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
 
-io.on("connection", (socket: Socket) => {
-	logger.info("Websocket connection established", { id: socket.id });
-	events.on("newHookRequest", (id, hookRequest) => {
-		socket.emit("events", hookRequest);
+nextApp.prepare()
+	.then(() => {
+		app.get("*", (req, res) => {
+			return handle(req, res);
+		});
+
+		app.get("/test", (req, res) => {
+			return nextApp.render(req, res, "/index", req.query);
+		});
+
+		const server = http.createServer(app);
+		const io = require("socket.io")(server);
+
+		io.on("connection", (socket: Socket) => {
+			logger.info("Websocket connection established", { id: socket.id });
+			events.on("newHookRequest", (id, hookRequest) => {
+				socket.emit("events", hookRequest);
+			});
+
+			socket.on("disconnect", () => {
+				logger.info("Websocker connection disconnected", { id: socket.id });
+			});
+		});
+
+		const gracefullyShutdown = () => {
+			server.close(() => {
+				// gracefully shutdown third party services;
+				events.removeAllListeners();
+				app.get("mysql").close();
+				logger.info("Server shutting down");
+				process.exit();
+			});
+		};
+
+		["SIGTERM", "SIGINT"].forEach((event: any) => {
+			process.on(event, gracefullyShutdown);
+		});
+
+		const port = app.get("port");
+		const env = app.get("env");
+
+		server.listen(port, () => {
+			logger.info(`App env: ${env}}`);
+			logger.info(`App is running at http://localhost:${port}`);
+			logger.info("Press CTRL-C to stop");
+		});
+	}).catch((ex) => {
+		logger.error(ex.stack);
+		process.exit(1);
 	});
-
-	socket.on("disconnect", () => {
-		logger.info("Websocker connection disconnected", { id: socket.id });
-	});
-});
-
-export const gracefullyShutdown = () => {
-	server.close(() => {
-		// gracefully shutdown third party services;
-		events.removeAllListeners();
-		app.get("mysql").close();
-		logger.info("Server shutting down");
-		process.exit();
-	});
-};
-
-["SIGTERM", "SIGINT"].forEach((event: any) => {
-	process.on(event, gracefullyShutdown);
-});
-
-const port = app.get("port");
-const env = app.get("env");
-
-server.listen(port, () => {
-	logger.info(`App env: ${env}}`);
-	logger.info(`App is running at http://localhost:${port}`);
-	logger.info("Press CTRL-C to stop");
-});
 
 process.on("uncaughtException", exception => {
 	logger.error("Uncaugh exception", { exception });
